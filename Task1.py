92191fa1e21ed5b26d6bcec9816d37fa2180a34d
Olesia Vysotina <vysotina2002@gmail.com>
2024-12-29 22:35:49 +0100

    use black-formatter

--- Task1.py
+++ Task1.py
@@ -1,15 +1,16 @@
 from abc import ABC, abstractmethod
 import logging
 
-logger = logging.getLogger() 
+logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 ch.setFormatter(formatter)
 logger.addHandler(ch)
+
 
 class Vehicle(ABC):
     def __init__(self, make, model):
@@ -20,6 +21,7 @@ class Vehicle(ABC):
     def start_engine(self) -> None:
         pass
 
+
 class VehicleFactory(ABC):
     @abstractmethod
     def create_car(self) -> Vehicle:
@@ -29,35 +31,40 @@ class VehicleFactory(ABC):
     def create_motorcycle(self) -> Vehicle:
         pass
 
+
 class Car(Vehicle):
-    def __init__(self, make:str, model:str, country:str):
+    def __init__(self, make: str, model: str, country: str):
         super().__init__(make, model)
         self.country = country
 
     def start_engine(self) -> None:
         logger.info(f"{self.make} {self.model} ({self.country}): Двигун запущено")
 
+
 class Motorcycle(Vehicle):
-    def __init__(self, make:str, model:str, country:str):
+    def __init__(self, make: str, model: str, country: str):
         super().__init__(make, model)
         self.country = country
 
     def start_engine(self) -> None:
         logger.info(f"{self.make} {self.model} ({self.country}): Мотор заведено")
 
+
 class USVehicleFactory(VehicleFactory):
-    def create_car(self, make:str, model:str) -> Vehicle:
+    def create_car(self, make: str, model: str) -> Vehicle:
         return Car(make, model, "US Spec")
 
-    def create_motorcycle(self, make:str, model:str) -> Vehicle:
+    def create_motorcycle(self, make: str, model: str) -> Vehicle:
         return Motorcycle(make, model, "US Spec")
 
+
 class EUVehicleFactory(VehicleFactory):
-    def create_car(self, make:str, model:str) -> Vehicle:
+    def create_car(self, make: str, model: str) -> Vehicle:
         return Car(make, model, "EU Spec")
 
-    def create_motorcycle(self, make:str, model:str) -> Vehicle:
+    def create_motorcycle(self, make: str, model: str) -> Vehicle:
         return Motorcycle(make, model, "EU Spec")
+
 
 # Використання
 us_factory = USVehicleFactory()
--- Task2.py
+++ Task2.py
@@ -1,14 +1,16 @@
 from abc import ABC, abstractmethod
 
+
 class Book:
-    def __init__(self, title:str, author:str, year:int):
+    def __init__(self, title: str, author: str, year: int):
         self.title = title
         self.author = author
         self.year = year
 
+
 class CheckInfo:
     @staticmethod
-    def get_year(number:str) -> int:
+    def get_year(number: str) -> int:
         while True:
             try:
                 year = int(number)
@@ -17,11 +19,12 @@ class CheckInfo:
                 print("Year should be a number")
                 number = input("Enter book year: ").strip()
 
+
 class LibraryInterface(ABC):
     @abstractmethod
     def add_book(self):
         pass
-    
+
     @abstractmethod
     def remove_book(self):
         pass
@@ -30,48 +33,53 @@ class LibraryInterface(ABC):
     def show_books(self):
         pass
 
+
 class Library(LibraryInterface):
     def __init__(self):
         self.books = []
 
     def add_book(self, book: Book):
         self.books.append(book)
-        print(f'Title: {book.title}, Author: {book.author}, Year: {book.year} was added')
+        print(
+            f"Title: {book.title}, Author: {book.author}, Year: {book.year} was added"
+        )
 
-    def remove_book(self, title:str):
+    def remove_book(self, title: str):
         for book in self.books:
             if book.title == title:
                 self.books.remove(book)
-                print(f'{title} was removed')
+                print(f"{title} was removed")
                 break
             else:
-                print(f'{title} not found')
-        
+                print(f"{title} not found")
+
     def show_books(self):
         if not self.books:
-            print('Library is empty')
+            print("Library is empty")
         else:
             for book in self.books:
-                print(f'Title: {book.title}, Author: {book.author}, Year: {book.year}')
+                print(f"Title: {book.title}, Author: {book.author}, Year: {book.year}")
+
 
 class LibraryManager:
     def __init__(self, library: Library):
         self.library = library
 
-    def add_book(self, title:str, author:str, year:int):
+    def add_book(self, title: str, author: str, year: int):
         book = Book(title, author, year)
         self.library.add_book(book)
-    
-    def remove_book(self, title:str):
+
+    def remove_book(self, title: str):
         self.library.remove_book(title)
 
     def show_books(self):
         self.library.show_books()
 
+
 def main():
     library = Library()
     manager = LibraryManager(library)
-    
+
     while True:
         command = input("Enter command (add, remove, show, exit): ").strip().lower()
 
@@ -90,6 +98,7 @@ def main():
                 break
             case _:
                 print("Invalid command. Please try again.")
+
 
 if __name__ == "__main__":
     main()
